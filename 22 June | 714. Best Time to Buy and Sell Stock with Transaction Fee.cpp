b81eb37dd1903b2c396885b817e62af3720e8650
SumitPareek2401 <122019171+SumitPareek2401@users.noreply.github.com>
2023-07-31 13:23:37 +0530

    Create 22 June | 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp

---
+++ 22 June | 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int buy = INT_MIN;
+        int sell = 0;
+        for(auto i : prices){
+            buy = max(buy, sell - i);
+            sell = max(sell, buy+i-fee);
+        }
+        return sell;
+    }
+};
